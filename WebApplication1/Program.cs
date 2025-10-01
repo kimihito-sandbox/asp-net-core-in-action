7b11ac4383191b1428dfa6fb1004072ac0a2da3f
kimihito <tatsurotamashiro@gmail.com>
2025-10-01 16:08:07 +0900

    5.2.2

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,15 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+var people = new List<Person>
 {
-    app.UseExceptionHandler("/Error");
-}
-
-app.UseStaticFiles();
-app.UseRouting();
+  new("Tom", "Hanks"),
+  new("Denzel", "Washington"),
+  new("Leonardo", "DiCaprio"),
+  new("Al", "Pacino"),
+  new("Morgan", "Freeman")
+};
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/error", () => "Sorry, error occurred.");
+app.MapGet("/person", () => new Person("Andrew", "Lock"));
+
+app.MapGet("/people/{name}", (string name) =>
+{
+  Console.WriteLine($"Received name: '{name}'");
+  return people.Where(p => p.FirstName.StartsWith(name, StringComparison.OrdinalIgnoreCase));
+});
 
 app.Run();
+
+public record Person(string FirstName, string LastName);
