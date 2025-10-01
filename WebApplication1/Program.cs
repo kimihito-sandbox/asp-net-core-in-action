a93b0b9a7feb587b56aec020b83ee2b95da3ea04
kimihito <tatsurotamashiro@gmail.com>
2025-10-01 15:33:26 +0900

    4.2.1

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,21 +1,8 @@
-using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpLogging(opts => opts.LoggingFields = HttpLoggingFields.RequestProperties);
-
-builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
-
 WebApplication app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-  app.UseHttpLogging();
-}
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/person", () => new Person("Andrew", "Lock"));
+app.UseMiddleware<WelcomePageMiddleware>();
 
 app.Run();
-
-public record Person(string FirstName, string LastName);
