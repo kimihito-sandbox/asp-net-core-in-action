c6911cb166cec81592b075af8147f115524e3ccf
kimihito <tatsurotamashiro@gmail.com>
2025-10-01 15:49:36 +0900

    4.2.3

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
 app.UseStaticFiles();
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/error", () => "Sorry, error occurred.");
 
 app.Run();
